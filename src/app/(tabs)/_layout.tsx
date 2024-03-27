af36e79e295d22277f3e88c631b5f691389056dd
JoaoVictorPimentel <joaovitorcavalcante009@gmail.com>
2024-03-26 21:20:48 -0300

    1.2

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -10,19 +10,19 @@ export default function Layout () {
       <Tabs screenOptions={{ headerShown: false, 
         tabBarStyle: {backgroundColor: '#111'}
       }}>
-        <Tabs.Screen name='(main)/Index' options={{
+        <Tabs.Screen name='(main)/index' options={{
           title: 'Home',
           tabBarIcon: (button) => <FontAwesome name='home' color={(button.focused ? '#0059A7' : 'white')}/>
         }}/>
-        <Tabs.Screen name='categories/Index' options={{
+        <Tabs.Screen name='categories/index' options={{
           title: 'Categorias',
           tabBarIcon: (button) => <FontAwesome name='navicon' color={(button.focused ? '#0059A7' : 'white')}/>
         }}/>
-        <Tabs.Screen name='cart/Index' options={{
+        <Tabs.Screen name='cart/index' options={{
           title: 'Carrinho',
           tabBarIcon: (button) => <FontAwesome name='shopping-cart' color={(button.focused ? '#0059A7' : 'white')}/>
         }}/>
-        <Tabs.Screen name='login/Index' options={{
+        <Tabs.Screen name='login/index' options={{
           title: 'Conta',
           tabBarIcon: (button) => <FontAwesome name='user' color={(button.focused ? '#0059A7' : 'white')}/>
         }}/>
